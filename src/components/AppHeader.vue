5211e3087bf0951e29674daa3a95245e5c6e20f2
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-02-12 14:07:27 +0100

    fixed the last files and started with the header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+    export default{
+        name: "AppHeader"
+    }
+</script>
+
+<template>
+    <header>
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+<style scoped lang="scss">
+  h1 {
+    margin: 0 auto;
+    width: 80%;
+    padding-block: 10px;
+  }
+</style>
